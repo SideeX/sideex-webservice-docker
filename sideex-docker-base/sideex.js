f99fc83fc704ccf8f12bef49ebbab9b5d42298c4
kiam <kiam125@gmail.com>
2020-09-22 22:45:40 +0800

    Others: change sideex download method

--- sideex-docker-base/sideex.js
+++ sideex-docker-base/sideex.js
@@ -2,17 +2,12 @@ const { exec, execSync, spawn } = require('child_process');
 
 let version = "3.3.12";
 exec(`npm i -g @sideex/webservice@${version}`, (err, stdout, stderr) => {
-  console.log(stdout.trim())
   if(err) {
-    exec(`npm view @sideex/webservice@${version} version -json`, (err, stdout, stderr) => {
-      if(stdout.trim() != "") {
-        exec(`npm view "@sideex/webservice@>=${version}-1 <${version}" version --json`, (err, stdout, stderr) => {
-          const versions = JSON.parse(stdout);
-          exec(`npm i @sideex/webservice@${versions[versions.length-1]}`, (err, stdout, stderr) => {
-            console.log(stdout);
-          });
-        });
-      }
+    exec(`npm view "@sideex/webservice@>=${version}-1 <${version}" version --json`, (err, stdout, stderr) => {
+      const versions = JSON.parse(stdout);
+      exec(`sudo npm i -g @sideex/webservice@${versions[versions.length-1]}`, (err, stdout, stderr) => {
+        console.log(stdout);
+      });
     });
   }
 });
