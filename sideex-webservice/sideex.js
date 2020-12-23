558789de53d7a74158d7a1b96f60c1c568985858
kiam <kiam125@gmail.com>
2020-12-24 02:39:25 +0800

    Refactor: change install new version

--- sideex-webservice/sideex.js
+++ sideex-webservice/sideex.js
@@ -1,17 +1,25 @@
 const fs = require('fs'); 
-const { exec, execSync, spawn } = require('child_process');
+const { exec } = require('child_process');
 
-let version = fs.readFileSync('/opt/sideex-webservice/version', 'utf8').trim();
+const varsionPath = "/opt/sideex-webservice/version.json";
+let version = JSON.parse(fs.readFileSync(varsionPath, 'utf8').trim());
+let finalVersion = version.finalVersion;
+let currentVersion = version.currentVersion;
+let first = version.first;
 
-exec(`npm i -g @sideex/webservice@${version}`, (err, stdout, stderr) => {
-  if(err) {
-    exec(`npm view "@sideex/webservice@>=${version}-1 <${version}" version --json`, (err, stdout, stderr) => {
-      const versions = JSON.parse(stdout);
-      console.log(`Downloading @sideex/webservice@${versions}....`);
-      fs.writeFileSync('/opt/sideex-webservice/version', versions);
-      exec(`sudo npm i -g @sideex/webservice@${versions}`, (err, stdout, stderr) => {
-        console.log(stdout);
-      });
+if(currentVersion != finalVersion) {
+    exec(`npm view "@sideex/webservice@ >= ${currentVersion} < ${finalVersion}" version --json`, (err, stdout, stderr) => {
+        const remoteVersions = JSON.parse(stdout);
+        const remoteVersion = typeof remoteVersions === "object" ? remoteVersions[remoteVersions.length-1]: remoteVersions;
+        
+        if(remoteVersion != currentVersion || first) {
+            version.currentVersion = remoteVersion;
+            version.first = false;
+            console.log(`Downloading @sideex/webservice@${remoteVersion}....`);
+            fs.writeFileSync(varsionPath, JSON.stringify(version, null, 4));
+            exec(`sudo npm i -g @sideex/webservice@${remoteVersion}`, (err, stdout, stderr) => {
+                console.log(stdout);
+            })
+        }
     });
-  }
-});
+}
